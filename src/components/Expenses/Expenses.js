e44c4d8bfd432c09ede4ed2cd91aab0a06b85adf
quechulo <@>
2023-02-23 21:36:37 +0100

    finished section 4

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = (props) => {
+  const [year, setFilteredYear] = useState('2023')
+  const changeFilterYear = (yearSelected) => {
+    setFilteredYear(yearSelected);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter yearSelected={year} onChangeFilter={changeFilterYear} />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
